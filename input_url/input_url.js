93584198b1e04de2376422cf669cac0b5e75d820
kulraj <kulraj@vinsol.com>
2013-06-19 19:22:14 +0530

    url input

---
+++ input_url/input_url.js
@@ -0,0 +1,32 @@
+var url;
+
+function openUrl() {
+    "use strict";
+    /*jslint browser: true*/
+    window.open(url, "mywindow", "width=400px,height=450px");
+}
+
+
+function urlInput() {
+    "use strict";
+    /*global prompt: true*/
+    /*global alert: true*/
+    var i, flag = 0;
+    url = prompt("enter url");
+    for (i = 0; i <  url.length; i += 1) {
+        if (url[i] !== " ") {
+            flag = 1;
+        }
+    }
+    if (!flag) {
+        alert("empty url");
+        urlInput();
+    }
+}
+
+
+function initialize() {
+    "use strict";
+    urlInput();
+    openUrl();
+}
