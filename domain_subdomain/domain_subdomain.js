9377b7b702c07bf7233dc3a6f03026f1d69e4b0e
kulraj <kulraj@vinsol.com>
2013-06-27 11:31:32 +0530

    variable name changed

--- domain_subdomain/domain_subdomain.js
+++ domain_subdomain/domain_subdomain.js
@@ -2,7 +2,7 @@
 function domainName(urlObject) {
     "use strict";
     /*global alert: true*/
-    var regex_for_link, regex_for_domain_subdomain, link, domain, subdomain, message;
+    var regex_for_link, regex_for_domain_subdomain, link, domain, subdomain, message, pattern_match;
 
     regex_for_link = "(http|ftp|https)?(?:://)?([^/]+)/?";
 
@@ -10,13 +10,13 @@ function domainName(urlObject) {
     link = RegExp.$2;
 
     regex_for_domain_subdomain = "([^/]*)[.]([^.]+.[^.]{2,4})$";
-    domain = link.match(regex_for_domain_subdomain);
-    if (!domain) {  //given pattern does not match means no subdomain
+    pattern_match = link.match(regex_for_domain_subdomain);
+    if (!pattern_match) {  //given pattern does not match means no subdomain
         domain = link;
         alert("domain: " + domain);
         return;
     }
-
+    //pattern matches then fresh values come in Regexp object
     domain = RegExp.$2;
     message = "domain: ";
     message += domain;
